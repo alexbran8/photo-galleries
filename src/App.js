5353fe6a7172b67f14e4ca26ce23bfd2f5fe8898
abran <alexandru.bran@nokia.com>
2022-07-03 16:54:53 +0300

    gallery #1

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ function App() {
   const clientId = "zE8Q8ZqNjUjnIuUtZ2fODg9_qoPgHPv33ppYfsojSbg";
   const [result, setResult] = useState([]);
 
-useEffect(()=>{
-  console.log(image)
-  handleSubmit();
-
-},[])
+  useEffect(() => {
+    handleSubmit();
+  }, [])
 
 
 
@@ -21,41 +19,44 @@ useEffect(()=>{
   };
 
   const handleSubmit = () => {
-    
-    if (image === '' ) {
+
+    if (image === '') {
       var imageToSearch = "nature"
     }
     else {
       imageToSearch = image
     }
 
-    
-    const url = "https://api.unsplash.com/search/photos?page=1&query=" + imageToSearch + "&client_id=" + clientId; axios.get(url).then((response) => {
+
+    const url = "https://api.unsplash.com/search/photos?per_page=999&query=" + imageToSearch + "&client_id=" + clientId;
+    // const url = "https://unsplash.com/collections/1163637/furniture" + imageToSearch + "&client_id=" + clientId;
+    axios.get(url).then((response) => {
       console.log(response);
       setResult(response.data.results);
     });
   };
 
 
-  return (<div className="app">
+  return (
+  <div className="app">
     <div className="heading">
-      <h1>React Image Search Using Unsplash API.</h1>
+      <h1>Gallery Template #1</h1>
     </div>
 
     <div className="input">
       <input onChange={handleChange} type="text" name="image" placeholder="Search for images" />
-    </div>  <button onClick={handleSubmit} type="submit">Search</button><div className="result">
-      {result.map((image) => (
-        <>
-          {console.log(image)}
-          <div className="card">
-            <img src={image.urls.thumb}  alt={image.urls}/>
-            <p className="username"> Photo by {image.user.name}</p>
-            <p className="like">👍 {image.likes}</p>
-          </div>
-        </>
-      ))}
-    </div></div>
+        </div>  <button onClick={handleSubmit} type="submit">Search</button><div className="result">
+          {result.map((image) => (
+            <>
+              <div className="card">
+                <img src={image.urls.thumb} alt={image.urls} />
+                <p className="username">Photo by {image.user.name}</p>
+                <p className="like">👍 {image.likes}</p>
+              </div>
+            </>
+          ))}
+        </div>
+  </div>
   );
 }
 
